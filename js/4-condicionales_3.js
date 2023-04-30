8d3de33efce896d244ef9e61fd722d8c3186bc60
priscilaGarciaMoya <priscilagarciamoya@alu.frt.utn.edu.ar>
2023-04-29 22:41:29 -0300

    se agrega ejercicio de condicionales

---
+++ js/4-condicionales_3.js
@@ -0,0 +1,5 @@
+/**
+ * al escribir una provincia de donde es el usuario, mostramos por consola algo respectivo de ella
+ */
+
+let provincia = prompt('Ingrese su provincia...').toLocaleLowerCase();
